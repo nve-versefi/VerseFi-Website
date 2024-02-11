e68bff034367b310a0a90cac8bc7e932f1455aa2
nve-versefi <n.vaneerde@versefi.co.uk>
2024-02-11 20:34:36 +0000

    Change to page.tsx

---
+++ app/(default)/page.tsx
@@ -0,0 +1,13 @@
+// page.tsx
+import React from 'react';
+
+const Page = () => {
+    return (
+        <div>
+            <h1>This is a page</h1>
+            <p>Welcome to my Next.js page!</p>
+        </div>
+    );
+};
+
+export default Page;
