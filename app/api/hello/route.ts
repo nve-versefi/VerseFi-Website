a79197581c3af1fba7ae9d4710bdc0646f85112d
nve-versefi <n.vaneerde@versefi.co.uk>
2024-02-11 21:17:51 +0000

    Amend route.tsx

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,15 +1,6 @@
-import { NextApiRequest, NextApiResponse } from 'next'
+// pages/api/hello.ts
+import type { NextApiRequest, NextApiResponse } from 'next'
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'https://default-api-url.com'; // Use the environment variable or a default value
-
-  try {
-    // Make your API call using the apiUrl variable
-    // For example:
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-    res.status(200).json(data);
-  } catch (error) {
-    res.status(500).json({ error: 'Error fetching data from API' });
-  }
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.status(200).json({ name: 'VerseFi Partners' })
 }
